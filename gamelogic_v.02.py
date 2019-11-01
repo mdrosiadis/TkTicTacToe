b92ad89370208e103872230005395475f5c1a48e
AlexisFil <dydimos88@gmail.com>
2019-11-02 01:20:34 +0200

    Second version

--- gamelogic_v.02.py
+++ gamelogic_v.02.py
@@ -1,23 +1,25 @@
 class Gameboard:
     '''A gameboard for TicTacToe'''
     def __init__( self,name,bdata,score):
-        sef.name=name
-        sef.bdata = bdata
+        self.name=name
+        self.bdata = bdata
         self.score = [0,0]
 
 
     def victory(self,symbol):
         victory = False
-        a1=[[0][0],[0][1],[0][2]]
-        a2=[[1][0],[1][1],[1][2]]
-        a3=[[2][0],[2][1],[2][2]] 
-        a4=[[0][0],[1][0],[2][0]]
-        a5=[[0][1],[1][1],[2][1]]
-        a6=[[0][2],[1][2],[2][2]]  
-        a7=[[0][0],[1][1],[2][2]]
-        a8=[[0][2],[1][1],[2][0]]
-        a=[a1,a2,a3,a4,a5,a6,a7,a8]
-        if [turn,turn,turn] in a:
+        s1=self.bdata[0][0]
+        s2=self.bdata[0][1]
+        s3=self.bdata[0][2]
+        s4=self.bdata[1][0]
+        s5=self.bdata[1][1]
+        s6=self.bdata[1][2]
+        s7=self.bdata[2][0]
+        s8=self.bdata[2][1]
+        s9=self.bdata[2][2]
+
+        a=[[s1,s2,s3],[s4,s5,s6],[s7,s8,s9],[s1,s4,s7],[s2,s5,s8],[s3,s6,s9],[s1,s5,s9],[s3,s5,s7]]
+        if [symbol,symbol,symbol] in a:
             victory = True
         return victory
 
@@ -35,14 +37,10 @@ class Gameboard:
 
     def display(self):
         for i in range (3):
-            for j in range(3):
-                print(self.bdata[i][j],end = ' ')
+                print(self.bdata[i][0],self.bdata[i][1],self.bdata[i][2],sep = '\t')
 
- def erase_board():
-     a = []
-     for i in range (3):
-         for j in range(3):
-             a[i][j] = j
+def erase_board():
+    a = [1,2,3],[4,5,6],[7,8,9]
     return a
             
 
@@ -50,20 +48,20 @@ class Gameboard:
 a=input("Δώσε όνομα του παίχτη 1")
 b = input ("Δώσε όνομα του παίχτη 2")
 board = erase_board()
-game = Gameboard([a,b='Computer'],board,[0,0])
+game = Gameboard([a,b],board,[0,0])
 while True:
     game.display()
     game.setpos('O')
-    game.victory('O')
+    victory = game.victory('O')
     if victory is True:
-        print(f'Ο {} νίκησε'.format(game.player_name[0]))
+        print('Ο {} νίκησε'.format(game.player_name[0]))
         game.score[0]+=1
         break
     game.display()
     game.setpos('X')
-    game.victory('X')
+    victory=game.victory('X')
     if victory is True:
-        print(f'Ο {} νίκησε'.format(game.player_name[1]))
+        print('Ο {} νίκησε'.format(game.player_name[1]))
         game.score[1]+=1
         break
     
