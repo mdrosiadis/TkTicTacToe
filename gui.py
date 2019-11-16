c781505d23f80112300fbf1e8702112325699200
Mike Drosiadis <mdrosiadis@gmail.com>
2019-11-16 11:02:45 +0200

    tkinter button example/ callback / function pointers

---
+++ gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from random import random
+
+def say_hello_plus(a):
+    print("Hello" + a)
+
+win = tk.Tk()
+
+b1 = tk.Button(win, text = "Hi", command = )
+b1.pack()
+
+win.mainloop()
+
