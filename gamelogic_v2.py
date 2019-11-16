934840bb635fd31c234dff129ace37461dddbf31
Mike Drosiadis <mdrosiadis@gmail.com>
2019-11-16 10:15:54 +0200

    Game logic seems to work

--- gamelogic_v2.py
+++ gamelogic_v2.py
@@ -20,37 +20,28 @@ class Gameboard:
           2   |   |   
        
         '''
-        
-        
-        #self.score = [0,0]
-        
+                
         self.scores = {'X' : 0, 'O' : 0}
         self.activeS = 'X'
         
         # more to come
-
-
+        
     def victory(self, symbol = None):
-        """
-        victory = False
-        s1=self.bdata[0][0]
-        s2=self.bdata[0][1]
-        s3=self.bdata[0][2]
-        s4=self.bdata[1][0]
-        s5=self.bdata[1][1]
-        s6=self.bdata[1][2]
-        s7=self.bdata[2][0]
-        s8=self.bdata[2][1]
-        s9=self.bdata[2][2]
-
-        a=[[s1,s2,s3],[s4,s5,s6],[s7,s8,s9],[s1,s4,s7],[s2,s5,s8],[s3,s6,s9],[s1,s5,s9],[s3,s5,s7]]
-        if [symbol,symbol,symbol] in a:
-            victory = True
-        return victory
-        """
         if symbol is None:
             symbol = self.activeS
 
+        if self.check_lines(symbol):
+            self.scores[symbol] += 1
+            return True
+        
+        return False
+    
+    def check_lines(self, symbol):
+        
+        """
+        if symbol is None:
+            symbol = self.activeS
+		"""
     
         # vertical lines:
         for x in range(self.size):
@@ -83,16 +74,6 @@ class Gameboard:
 
     def setpos(self, pos, symbol = None):
         # pos -> tuple (int , int) 0-2 as for x , y
-        """
-        while True:
-            pos = input ("Position")
-            if str(self.bdata[pos]) is True:
-                print ("Position already selected")
-            else:
-                self.bdata[pos] = symbol
-                break
-        return self.bdata
-        """
         if symbol is None:
             symbol = self.activeS
         
@@ -121,11 +102,6 @@ class Gameboard:
     def next_player(self):
       self.activeS = 'X' if self.activeS == 'O' else 'O'
 
-"""
-def erase_board():
-    a = [1,2,3],[4,5,6],[7,8,9]
-    return a
-"""
 
 #Initiation script containing script to be removed from testing
 a = input("Δώσε όνομα του παίχτη 1: ")
@@ -147,5 +123,4 @@ while True:
         game.next_player()
 
             
-        
-    
+
